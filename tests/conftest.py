0ec36a3de328963df20cebbf6e5e4b5813461dd5
Kirill Sumorokov <sumorokov.k@gmail.com>
2023-10-15 23:23:41 +0500

    Added some smoke tests

---
+++ tests/conftest.py
@@ -0,0 +1,33 @@
+from importlib.metadata import Distribution, distribution
+
+import punq
+import pytest
+from aiohttp import web
+from structlog.types import WrappedLogger
+
+from heimdall.app import create_container, init
+from heimdall.logging import configure_logging
+
+
+@pytest.fixture(scope="session")
+def dist() -> Distribution:
+    """Patch application distribution."""
+    return distribution("heimdall")
+
+
+@pytest.fixture()
+def logger(dist: Distribution) -> WrappedLogger:
+    """Configure logging for tests."""
+    return configure_logging(dist=dist, debug=False)
+
+
+@pytest.fixture()
+def container(logger: WrappedLogger) -> punq.Container:
+    """Create IoC container."""
+    return create_container(logger=logger)
+
+
+@pytest.fixture()
+def app(dist: Distribution, container: punq.Container) -> web.Application:
+    """Prepare test application."""
+    return init(dist=dist, container=container, debug=False)
